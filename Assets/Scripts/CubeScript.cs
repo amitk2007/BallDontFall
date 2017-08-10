178f4f28434906a562016dde8c4802c229960625
amitk2007 <amitk2007@gmail.com>
2017-08-10 17:36:24 +0300

    beta1
    try first map and movment

---
+++ Assets/Scripts/CubeScript.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeScript : MonoBehaviour
+{
+
+    public enum CubeState
+    {
+        StartCube,
+        EndCube,
+        GoodCube,
+        BadCube
+    }
+
+    public CubeState cubeState;
+
+    // Use this for initialization
+    void Start()
+    {
+        switch (cubeState)
+        {
+            case CubeState.StartCube:
+                ChangeColorTo(Color.yellow);
+                break;
+            case CubeState.EndCube:
+                ChangeColorTo(Color.blue);
+                break;
+            case CubeState.GoodCube:
+                ChangeColorTo(Color.green);
+                break;
+            case CubeState.BadCube:
+                ChangeColorTo(Color.red);
+                break;
+            default:
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ChangeColorTo(Color color)
+    {
+        //this.GetComponent<Renderer>().material.SetColor("color",color);
+        //gameObject.renderer.material.color = new Color(1, 1, 1);
+        this.GetComponent<Renderer>().material.color = color;
+
+    }
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour
+{
+    public GameObject startCube;
+
+    Color startCubeColor;
+    GameObject[] allCubs;
+
+    // Use this for initialization
+    void Start()
+    {
+        //startCubeColor = Color.yellow;
+        //allCubs = GameObject.FindGameObjectsWithTag("Cube");
+        //this.gameObject.transform.position = GetSpawnPointFromCubesArray(allCubs, startCubeColor);
+        this.gameObject.transform.position = GetSpwanPointFromStartCube(startCube);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Input.acceleration.x, 0, -Input.acceleration.z);
+    }
+
+    public Vector3 GetSpwanPointFromStartCube(GameObject startCube)
+    {
+        Vector3 point = startCube.transform.position;
+        point.y++;
+        return point;
+    }
+
+    public Vector3 GetSpawnPointFromCubesArray(GameObject[] cubesArray, Color startCubeColor)
+    {
+        Vector3 point = new Vector3(0, 0, 0);
+
+        foreach (GameObject cube in cubesArray)
+        {
+            if (cube.GetComponent<Renderer>().material.color == startCubeColor)
+            {
+                point = cube.transform.position;
+            }
+        }
+        point.y++;
+        return point;
+    }
+}
